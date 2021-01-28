14a8ea89d289daa35e423bae00f3dddfd97b3b47
Dongjin Kim <tobetter@gmail.com>
2021-01-28 14:49:05 +0900

    Fix 'update.sh' to update changelog and git
    Signed-off-by: Dongjin Kim <tobetter@gmail.com>

--- updates.sh
+++ updates.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+export DEBNAME="Dongjin Kim"
+export DEBEMAIL="tobetter@gmail.com"
+
 PACKAGE_FILE="package.zip"
 
 version=$(curl -sL https://api.github.com/repos/meteyou/mainsail/releases/latest \
@@ -11,3 +14,9 @@ wget ${package} -O ${PACKAGE_FILE}
 rm -rf var/www/mainsail && mkdir -p var/www/mainsail
 unzip ${PACKAGE_FILE} -d var/www/mainsail
 rm -f ${PACKAGE_FILE}
+
+dch -v $(echo ${version} |  sed "s/[^0-9]*//") "Package version bump to ${version}"
+
+git add var
+git add debian/changelog
+git commit -s -m "Package version bump to ${version}"
